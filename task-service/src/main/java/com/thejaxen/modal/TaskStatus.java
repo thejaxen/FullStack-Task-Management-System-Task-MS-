6cb3a8be9ae25a06e5c93fc7f372bca2b3dbe18e
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2025-01-29 08:56:50 +0300

    Configurations
    Configurations are done due to the task service requirements. Entity is written and due to task status enum class is written too.

---
+++ task-service/src/main/java/com/thejaxen/modal/TaskStatus.java
@@ -0,0 +1,12 @@
+package com.thejaxen.modal;
+
+public enum TaskStatus {
+
+    PENDING("PENDING"),
+    ASSIGNED("ASSIGNED"),
+        DONE("DONE");
+
+    TaskStatus(String status) {
+
+    }
+}
---
+++ task-service/src/test/java/com/thejaxen/TaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thejaxen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
